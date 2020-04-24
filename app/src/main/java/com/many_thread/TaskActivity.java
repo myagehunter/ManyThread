556375236b4f456709ce6fb6d60fbfd86bd1471c
jianghuiz <1052560537@qq.com>
2020-04-24 15:02:54 +0800

    提交task

---
+++ app/src/main/java/com/many_thread/TaskActivity.java
@@ -0,0 +1,13 @@
+package com.many_thread;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+
+/**
+ * author : jianghuizhong
+ * date   : 2020/4/24
+ * desc   :
+ */
+
+public class TaskActivity extends Activity{
+}
